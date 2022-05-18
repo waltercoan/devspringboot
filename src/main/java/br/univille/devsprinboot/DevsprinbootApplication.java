a8596397bf8b5be48febc4294c8ca5c56ae205d1
waltercoan <walter.coan@gmail.com>
2022-05-18 09:27:39 -0300

    Mapeamento Objeto Relacional

---
+++ src/main/java/br/univille/devsprinboot/DevsprinbootApplication.java
@@ -0,0 +1,13 @@
+package br.univille.devsprinboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevsprinbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevsprinbootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/univille/devsprinboot/DevsprinbootApplicationTests.java
@@ -0,0 +1,13 @@
+package br.univille.devsprinboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevsprinbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
